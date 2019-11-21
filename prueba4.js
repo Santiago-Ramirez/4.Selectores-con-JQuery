126506d34c10d6d09bc19b082e0b23b37d54d917
santiRMZ <ubaldo_desantiago@hotmail.com>
2019-11-21 04:53:10 -0600

    commit's

--- prueba4.js
+++ prueba4.js
@@ -1,32 +1,43 @@
 $(document).ready(function(){
 
-
-//
-//Objeto Jquery  y objetos tienen atributos y metodos
-//objeto de Jquery .css
-
-
-  $("#x").click(function(){
-
+  $("#pinta1").click(function(){
     $("div").css("background-color", "white");
-
     $(".cuadro1").css("background-color", "red");
-
   });
-  $("#limpiar").click(function(){
 
+  $("#clean").click(function(){
 		$("div").css("background-color", "white");
-
     alert("Se ha limpiado con exito");
-
 	});
 
-  $("#y").click(function(){
 
+  $("#pinta2").click(function(){
     $("div").css("background-color", "white");
-
-    $("#h").css("background-color", "red");
-
+    $("#a,#b,#c,#d,#e").css("background-color", "red");
   });
+
+
+$("#pinta3").click(function(){
+  $("div").css("background-color", "white");
+  $(".cuadro2").css("background-color", "red");
+});
+
+
+$("#lazeta").click(function(){
+  $("div").css("background-color", "white");
+  $(".cuadro1,.cuadro2,#a,#b,#c,#d,#e").css("background-color", "red");
+});
+
+$("#pinta5").click(function(){
+  $("div").css("background-color", "white");
+  $(".cuadro1,.cuadro2,#a,#b,#c,#d,#f,#g,#h,#i,#j,#k,#l,#m").css("background-color", "red");
+});
+
+$("#pinta6").click(function(){
+  $("div").css("background-color", "white");
+  $("div").css("background-color", "red");
+  $(".alinearBTN,#white").css("background-color", "white");
+
+});
 
 });
